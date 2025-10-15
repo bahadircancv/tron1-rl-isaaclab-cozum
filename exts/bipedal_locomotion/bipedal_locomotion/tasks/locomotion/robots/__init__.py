3d6ff7ac25643288ac2efe8ccbd9bc254a764006
Bahadir Can <bcan@cozum-makina.com>
2025-10-15 11:54:32 +0300

    added rough env to training;

--- exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/robots/__init__.py
+++ exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/robots/__init__.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 
-from bipedal_locomotion.tasks.locomotion.agents.limx_rsl_rl_ppo_cfg import PF_TRON1AFlatPPORunnerCfg, WF_TRON1AFlatPPORunnerCfg, SF_TRON1AFlatPPORunnerCfg
+from bipedal_locomotion.tasks.locomotion.agents.limx_rsl_rl_ppo_cfg import PF_TRON1AFlatPPORunnerCfg, WF_TRON1AFlatPPORunnerCfg, WF_TRON1ARoughPPORunnerCfg, SF_TRON1AFlatPPORunnerCfg
 
 from . import limx_pointfoot_env_cfg, limx_wheelfoot_env_cfg, limx_solefoot_env_cfg
 
@@ -11,6 +11,7 @@ from . import limx_pointfoot_env_cfg, limx_wheelfoot_env_cfg, limx_solefoot_env_
 limx_pf_blind_flat_runner_cfg = PF_TRON1AFlatPPORunnerCfg()
 
 limx_wf_blind_flat_runner_cfg = WF_TRON1AFlatPPORunnerCfg()
+limx_wf_blind_rough_runner_cfg = WF_TRON1ARoughPPORunnerCfg()
 
 limx_sf_blind_flat_runner_cfg = SF_TRON1AFlatPPORunnerCfg()
 
@@ -43,6 +44,31 @@ gym.register(
     },
 )
 
+
+############################
+# PF Blind Rough Environment
+############################
+gym.register(
+    id="Isaac-Limx-PF-Blind-Rough-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": limx_pointfoot_env_cfg.PFBlindRoughEnvCfg,
+        "rsl_rl_cfg_entry_point": limx_pf_blind_flat_runner_cfg, #TODO implement rough_runner_cfg
+    },
+)
+
+gym.register(
+    id="Isaac-Limx-PF-Blind-Rough-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": limx_pointfoot_env_cfg.PFBlindRoughEnvCfg_PLAY,
+        "rsl_rl_cfg_entry_point": limx_pf_blind_flat_runner_cfg, #TODO implement rough_runner_cfg
+    },
+)
+
+
 #############################
 # WF Blind Flat Environment
 #############################
@@ -63,6 +89,30 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": limx_wheelfoot_env_cfg.WFBlindFlatEnvCfg_PLAY,
         "rsl_rl_cfg_entry_point": limx_wf_blind_flat_runner_cfg,
+    },
+)
+
+
+#############################
+# WF Blind Rough Environment
+#############################
+gym.register(
+    id="Isaac-Limx-WF-Blind-Rough-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": limx_wheelfoot_env_cfg.WFBlindRoughEnvCfg,
+        "rsl_rl_cfg_entry_point": limx_wf_blind_rough_runner_cfg,
+    },
+)
+
+gym.register(
+    id="Isaac-Limx-WF-Blind-Rough-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": limx_wheelfoot_env_cfg.WFBlindRoughEnvCfg_PLAY,
+        "rsl_rl_cfg_entry_point": limx_wf_blind_rough_runner_cfg,
     },
 )
 
